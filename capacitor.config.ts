516e3c7fb48f5a1b97fcdd267d5d7a585099f2fb
Jesus Castro <jesusdavidcastror30@gmail.com>
2025-03-01 09:05:58 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ECommerce_Parcial',
+  webDir: 'www'
+};
+
+export default config;
